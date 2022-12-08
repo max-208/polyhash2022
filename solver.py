8369435200d8e5b8ad5bbf296277a486fa8c000a
RAMIREZ <jdramirezo@unal.edu.co>
2022-12-08 12:51:28 +0100

    Feat: Modification de la fonction d'acceleration, On a ajouté la possiblite d'accelerer au maximum

--- solver.py
+++ solver.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from model import *
+from numpy import array
+from numpy.linalg import norm
 
 class SantaPoint:
     x: int
@@ -14,7 +16,7 @@ class SantaPoint:
 # creation d'un classe que transport le traineua d'un point a à un point b
 # objective: I need to move santa to A and B and change the internal parameters of the traineau
 class CheminSimple:
-    inicio: SantaPoint
+    inicio: SantaPoint 
     fin: SantaPoint
     santa: traineau
     delta_c: int
@@ -26,6 +28,45 @@ class CheminSimple:
         self.santa = s
         self.delta_c = b.x - a.x
         self.delta_r = b.y - a.y
+        
+    def acc_max(self)->list[float,float,bool,float,float,int,int]:
+        u=[self.delta_c,self.delta_r]/norm([self.delta_c,self.delta_r])
+        a=[u[0]*self.santa.accelerationUpperBound,u[1]*self.santa.accelerationUpperBound]
+        a2= [0,0,False,0,0,0,0]
+        if a[0]>self.delta_c:
+            a2[0] = self.delta_c
+            a2[2] = False
+            a2[5] = 1
+        elif a[0] % self.delta_c == 0:
+            a2[0] = a[0]
+            a2[2] = False
+            a2[5] = self.delta_c // a2[0]
+
+        else:
+            a2[0]= a[0]
+            a2[2]=True 
+            a2[5] = self.delta_c // a2[0]
+
+        if a[1]>self.delta_r:
+            a2[1]=self.delta_r
+            a2[2] = False
+            a2[6] = 1
+        elif a[1] % self.delta_r == 0:
+            a2[1] = a[1]
+            a2[2] = False
+            a2[6] = self.delta_r // a2[1]
+        else:
+            a2[1]= a[1]
+            a2[2]=True 
+            a2[6] = self.delta_r // a2[1]
+        
+        a2[3] = self.delta_c % a2[0]
+        a2[4] = self.delta_r % a2[1]
+        
+        print("the max acceleration possible is : ", a, a2)
+        return a2
+
+
 
     def acc_init(self):
         if self.delta_c >= 1:
@@ -74,7 +115,7 @@ class CheminSimple:
             self.santa.accelerer(1, "down")
         if self.delta_r <= -1:
             self.santa.accelerer(1, "up")
-    def tracker(self): #Afficher infor du traineau
+    def tracker(self):
         print(self.santa.getPoids(),self.santa.positionX,self.santa.positionY)
 
     def move(self):
@@ -96,9 +137,11 @@ class CheminSimple:
 
 
 
+
 P1 = SantaPoint(0, 0)
 P2 = SantaPoint(6, 6)
 S1 = traineau(3)
 S1.chargerCarotte(10)
 C1 = CheminSimple(P1, P2, S1)
 C1.move()
+C1.acc_max()
