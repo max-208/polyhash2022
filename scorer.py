05badcad8b1ed5408611b4877d8c3171c7e2ac9b
JanKo <jdramirezo@icloud.com>
2023-01-18 16:07:04 +0100

    Fix: Remplacement commande match par elif dans scorer.py

--- scorer.py
+++ scorer.py
@@ -2,7 +2,7 @@ from model import traineau,heatMap
 import fileParser
 
 def score(problemFineName, solutionFileName) -> int:
-	(cadeaux, timeLimit,reachrange,acccalc) = fileParser.parseChallenge(problemFineName)	
+	(cadeaux, timeLimit,reachrange,acccalc) = fileParser.parseChallenge(problemFineName)
 	instructions = fileParser.parseSolution(solutionFileName)
 	t = traineau(reachrange,acccalc)
 	map = heatMap(reachrange,cadeaux)
@@ -10,25 +10,24 @@ def score(problemFineName, solutionFileName) -> int:
 	score = 0
 	for instruction in instructions:
 		print(t.positionX,t.positionY,t.getPoids(),instruction)
-		match instruction[0]:
-			case "AccUp":
-				t.accelerer(instruction[1],"up")
-			case "AccDown":
-				t.accelerer(instruction[1],"down")
-			case "AccLeft":
-				t.accelerer(instruction[1],"left")
-			case "AccRight":
-				t.accelerer(instruction[1],"right")
-			case "Float":
-				t.flotter(instruction[1])
-				timeSpent += instruction[1]
-			case "LoadCarrots":
-				t.chargerCarotte(instruction[1])
-			case "LoadGift":
-				t.chargerCadeau(map.cadeaux.get(instruction[1],None))
-			case "DeliverGift":
-				t.livrerCadeau(map.cadeaux.get(instruction[1],None))
-				score += map.cadeaux.get(instruction[1],None).score
+		if instruction[0] == "AccUp":
+			t.accelerer(instruction[1],"up")
+		elif instruction[0] == "AccDown":
+			t.accelerer(instruction[1],"down")
+		elif instruction[0] == "AccLeft":
+			t.accelerer(instruction[1],"left")
+		elif instruction[0] == "AccRight":
+			t.accelerer(instruction[1],"right")
+		elif instruction[0] == "Float":
+			t.flotter(instruction[1])
+			timeSpent += instruction[1]
+		elif instruction[0] == "LoadCarrots":
+			t.chargerCarotte(instruction[1])
+		elif instruction[0] == "LoadGift":
+			t.chargerCadeau(map.cadeaux.get(instruction[1],None))
+		elif instruction[0] == "DeliverGift":
+			t.livrerCadeau(map.cadeaux.get(instruction[1],None))
+			score += map.cadeaux.get(instruction[1],None).score
 		if(timeSpent > timeLimit):
 			raise RuntimeWarning("Limite de temps dépassée : ", timeSpent, "/",timeLimit)
 	return score
--- test/test_chemin_multiple.py
+++ test/test_chemin_multiple.py
@@ -8,14 +8,14 @@ challenge = "f_festive_flyover.in.txt"
 
 currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parentdir = os.path.dirname(currentdir)
-sys.path.insert(0, parentdir) 
+sys.path.insert(0, parentdir)
 
-from model import cadeau,groupe, heatMap, chemin, boucle, parcoursFinal,traineau,accelerationCalculator
+from model import cadeau, groupe, heatMap, chemin, boucle, parcoursFinal, traineau, accelerationCalculator
 import fileParser
 
 (cadeaux, secondes, reachRange, accCalculator) = fileParser.parseChallenge(challenge)
 
-heatmap = heatMap(reachRange,cadeaux)
+heatmap = heatMap(reachRange, cadeaux)
 parcoursFinal = parcoursFinal()
 maxValRegion = 0
 while (secondes > 0 and maxValRegion != -math.inf):
@@ -26,82 +26,84 @@ while (secondes > 0 and maxValRegion != -math.inf):
 	maxRegion = None
 	for row in heatmap.regions:
 		for region in row:
-			#TODO : raffiner l'algo de décision
-			if(region.getScore() == 0):
+			# TODO : raffiner l'algo de décision
+			if (region.getScore() == 0):
 				val = -math.inf
 			else:
-				val = region.getPoids() * -20 + region.getScore()*100 -(distance.euclidean([0,0],[(region.maxX + region.minX) / 2, (region.maxY + region.minY) / 2 ] )//15)** 2
-				if(val > maxValRegion):
+				val = region.getPoids() * -20 + region.getScore() * 100 - (distance.euclidean([0, 0], [
+					(region.maxX + region.minX) / 2, (region.maxY + region.minY) / 2]) // 15) ** 2
+				if (val > maxValRegion):
 					maxValRegion = val
 					maxRegion = region
 	# pour cette région on prend le meilleur point
-	if(maxRegion != None):
+	if (maxRegion != None):
 		bcl = boucle()
-		previousGroup = groupe(0,0)
+		previousGroup = groupe(0, 0)
 		maxValGroupe = 0
-		while(maxValGroupe != -math.inf):
+		while (maxValGroupe != -math.inf):
 			maxValGroupe = -math.inf
-			maxGroup = groupe(0,0)
+			maxGroup = groupe(0, 0)
 			groups = maxRegion.getGroups()
 			for row in groups:
 				for group in row:
 					score = group.getScore()
-					if(score == 0 or (group.positionX == 0 and group.positionY == 0) or group.delivre == True):
+					if (score == 0 or (group.positionX == 0 and group.positionY == 0) or group.delivre == True):
 						val = -math.inf
 					else:
-						val = score*10 - group.getPoids()*2
-						if(val > maxValGroupe):
+						val = score * 10 - group.getPoids() * 2
+						if (val > maxValGroupe):
 							maxValGroupe = val
 							maxGroup = group
 
-			cheminRetour = chemin(maxGroup,previousGroup,reachRange,accCalculator,bcl.getPoids())
-			cheminAller = chemin(groupe(0,0),maxGroup,reachRange,accCalculator,bcl.getPoids() + cheminRetour.carotteConsommes + maxGroup.getPoids())
+			cheminRetour = chemin(maxGroup, previousGroup, reachRange, accCalculator, bcl.getPoids())
+			cheminAller = chemin(groupe(0, 0), maxGroup, reachRange, accCalculator,
+								 bcl.getPoids() + cheminRetour.carotteConsommes + maxGroup.getPoids())
 
-			#print(str(bcl))
-			if(secondes - (bcl.getTempsConsomme() + cheminRetour.tempsConsomme + cheminAller.tempsConsomme)  >= 0 and maxValGroupe != -math.inf):
+			# print(str(bcl))
+			if (secondes - (
+					bcl.getTempsConsomme() + cheminRetour.tempsConsomme + cheminAller.tempsConsomme) >= 0 and maxValGroupe != -math.inf):
 				previousGroup = maxGroup
-				if(len(bcl.chemins) != 0):
+				if (len(bcl.chemins) != 0):
 					pop = bcl.chemins.pop(0)
-				bcl.chemins.insert(0,cheminRetour)
-				bcl.chemins.insert(0,cheminAller)
+				bcl.chemins.insert(0, cheminRetour)
+				bcl.chemins.insert(0, cheminAller)
 				for cadeau in maxGroup.cadeaux:
-					if(not cadeau.enTransport):
+					if (not cadeau.enTransport):
 						cadeau.enTransport = True
 			else:
 				maxGroup.delivre = True
 				for cadeau in maxGroup.cadeaux:
 					cadeau.delivre = True
-	
-	
-		print("---",secondes,bcl.getPoids(),bcl.getScore(),len(bcl.chemins))
-		if(secondes - bcl.getTempsConsomme() >= 0):
+
+		print("---", secondes, bcl.getPoids(), bcl.getScore(), len(bcl.chemins))
+		if (secondes - bcl.getTempsConsomme() >= 0):
 			parcoursFinal.boucles.append(bcl)
 			secondes = secondes - bcl.getTempsConsomme()
-			lastGroup = groupe(0,0)
+			lastGroup = groupe(0, 0)
 			for c in bcl.chemins:
-				#print("(",c.begining.positionX,c.begining.positionY,")->(",c.end.positionX,c.end.positionY,")")
-				if(c.begining.positionX != lastGroup.positionX or c.begining.positionY != lastGroup.positionY):
+				# print("(",c.begining.positionX,c.begining.positionY,")->(",c.end.positionX,c.end.positionY,")")
+				if (c.begining.positionX != lastGroup.positionX or c.begining.positionY != lastGroup.positionY):
 					raise RuntimeWarning("chemin non cohérent")
 				lastGroup = c.end
 				c.move()
 				for cadeau in c.end.cadeaux:
-					if(not cadeau.delivre):
-						bcl.loadingActions.append(["LoadGift",cadeau.nom])
-						#print(str(cadeau),end="")
+					if (not cadeau.delivre):
+						bcl.loadingActions.append(["LoadGift", cadeau.nom])
+						# print(str(cadeau),end="")
 						cadeau.delivre = True
 			carrotCount = bcl.getCarotteConsommes()
-			if(carrotCount > 0):
-				bcl.loadingActions.append(["LoadCarrots",carrotCount])
+			if (carrotCount > 0):
+				bcl.loadingActions.append(["LoadCarrots", carrotCount])
 		else:
 			for c in bcl.chemins:
 				c.move()
 				for cadeau in c.end.cadeaux:
 					cadeau.delivre = True
-			
+
 print("----------------")
 print(secondes)
 with open("test.out.txt", 'w') as a_fichier:
-        contenu = a_fichier.write(str(parcoursFinal))
+	contenu = a_fichier.write(str(parcoursFinal))
 
 import os
 import sys
@@ -112,4 +114,5 @@ parentdir = os.path.dirname(currentdir)
 sys.path.insert(0, parentdir)
 
 import scorer
-print(scorer.score(challenge,"test.out.txt"))
+
+print(scorer.score(challenge, "test.out.txt"))
